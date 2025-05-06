9f969e23382574403a37dbcc353491f289024411
Martin Ivanov <marto1980@gmail.com>
2025-05-06 12:09:50 +0300

    Initialise Needed Components

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-investment-results',
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.scss',
+})
+export class InvestmentResultsComponent {}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-user-input',
+  imports: [],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.scss',
+})
+export class UserInputComponent {}
