5c7ba9b8ae1ac7966de42f6947e3bc809ec82c27
Martin Ivanov <marto1980@gmail.com>
2025-05-08 10:45:19 +0300

    Show Results on Submitting Form

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,17 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 import { INPUT_TYPES } from './user-input.model'
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.scss',
 })
 export class UserInputComponent {
   inputTypes = INPUT_TYPES
   @Output() calculate = new EventEmitter<void>()
-  onClick() {
+  onSubmit() {
     this.calculate.emit()
   }
 }
