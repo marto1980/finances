1a832b14dc0ccde904aca326a10b772390bff3f6
Martin Ivanov <marto1980@gmail.com>
2025-05-10 17:26:29 +0300

    Make calculateInvestmentResultsImmutable

---
+++ src/app/investment-results/investment-results.ts
@@ -0,0 +1,44 @@
+type InvestmentResult = {
+  year: number
+  interest: number
+  valueEndOfYear: number
+  annualInvestment: number
+  totalInterest: number
+  totalAmountInvested: number
+}
+
+const calculateInvestmentResults = (
+  initialInvestment: number,
+  annualInvestment: number,
+  expectedReturn: number,
+  duration: number,
+): readonly InvestmentResult[] => {
+  const helper = (
+    year: number,
+    investmentValue: number,
+    acc: readonly InvestmentResult[],
+  ): readonly InvestmentResult[] => {
+    if (year > duration) {
+      return acc
+    }
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100)
+    const newInvestmentValue =
+      investmentValue + interestEarnedInYear + annualInvestment
+    const totalInterest =
+      newInvestmentValue - annualInvestment * year - initialInvestment
+    const result: InvestmentResult = {
+      year,
+      interest: interestEarnedInYear,
+      valueEndOfYear: newInvestmentValue,
+      annualInvestment,
+      totalInterest,
+      totalAmountInvested: initialInvestment + annualInvestment * year,
+    }
+
+    return helper(year + 1, newInvestmentValue, [...acc, result])
+  }
+
+  return helper(1, initialInvestment, [])
+}
+
+export { calculateInvestmentResults }
