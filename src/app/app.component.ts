03dacdec6af11518b293d033108222ce58beec7a
Martin Aleksandrov Ivanov <marto@marto.OpenBSD>
2025-06-08 09:15:09 +0300

    Migrate App to NgModules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, inject } from '@angular/core'
 
-import { HeaderComponent } from './header/header.component'
 import { InvestmentResultsService } from './investment-results.service'
-import { InvestmentResultsComponent } from './investment-results/investment-results.component'
-import { UserInputComponent } from './user-input/user-input.component'
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
+  // eslint-disable-next-line @angular-eslint/prefer-standalone
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { InvestmentResultsComponent } from './investment-results/investment-results.component'
+import { UserInputComponent } from './user-input/user-input.component'
+
+@NgModule({
+  declarations: [AppComponent], // non-standalone components
+  bootstrap: [AppComponent],
+  imports: [
+    HeaderComponent,
+    UserInputComponent,
+    InvestmentResultsComponent,
+    BrowserModule,
+  ], // standalone components
+})
+export class AppModule {}
