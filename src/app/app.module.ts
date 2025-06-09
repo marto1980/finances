d1245161cf5add265efa1226b8f5d1f6e036cc50
Martin Aleksandrov Ivanov <marto@marto.OpenBSD>
2025-06-09 10:17:23 +0300

    Create a Separate UserInputModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,19 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
 import { InvestmentResultsComponent } from './investment-results/investment-results.component'
-import { UserInputComponent } from './user-input/user-input.component'
+import { UserInputModule } from './user-input/user-input.module'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    InvestmentResultsComponent,
-    UserInputComponent,
-  ], // non-standalone components
+  declarations: [AppComponent, HeaderComponent, InvestmentResultsComponent], // non-standalone components
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     CommonModule, // shared module
-    FormsModule, // reactive forms module
+    UserInputModule,
   ], // standalone components
 })
 export class AppModule {}
